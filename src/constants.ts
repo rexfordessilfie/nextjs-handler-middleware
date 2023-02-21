162cce1cc3bab42aae78daf62f60cfb6ffdf5a55
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-02-20 22:48:33 -0600

    feat: add createHandler

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+// Source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+/**
+ * List of http methods.
+ */
+export const HTTP_METHODS = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  PATCH: "PATCH",
+  DELETE: "DELETE",
+  HEAD: "HEAD",
+  OPTIONS: "OPTIONS",
+  TRACE: "TRACE",
+  CONNECT: "CONNECT",
+} as const;
